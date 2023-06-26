09c72c8dcb75aab8a0cb99adae63bf8ce1af6717
manar <mzzo3bi20988@gmail.com>
2023-06-26 16:05:09 +0300

    cards and css added

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -2,12 +2,15 @@ const mongoose =require('mongoose');
 
 const booksDchema ={
     title:String,
-    content : String
+    author:String,
+    content : String,
+    image:String,
+    price:String
 }
 
 const Book =mongoose.model('Book' ,booksDchema );
 
-// const testBook = new Book({title:"new book" , content:"bad book"});
+// const testBook = new Book({title:"new book" ,author:"me" ,content:"bad book", image:"gggggggggg" , price:"25"});
 // testBook.save().then(doc =>{console.log(doc)}).catch(err =>{console.log(err)});
 // console.log("saved");
 module.exports = Book;
--- frontend/src/components/Books.jsx
+++ frontend/src/components/Books.jsx
@@ -1,53 +1,77 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useState, useEffect } from "react";
-
-import { Table } from "react-bootstrap";
-
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Button, Row , Col} from "react-bootstrap";
+import './Books.css';
 function Books() {
   const [books, setBooks] = useState([
     {
       title: "",
+      author:"",
       content: "",
-    }
+      image:"",
+      price:""
+    },
   ]);
-
 
   useEffect(() => {
     axios
       .get("http://localhost:3001/books")
       .then(({ data }) => {
-        console.log("data   " , data)
-        setBooks(data);
+            setBooks(data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
-  
-  const DataTable = () => {
-    return books.map((res, i) => {
-      return <res obj={res} key={i} />;
-    });
-  };
-  
+
+  // const DataTable = () => {
+  //   return books.map((res, i) => {
+  //     return <res obj={res} key={i} />;
+  //   });
+  // };
+function handleBuyBtnClick(event){
+  const {title} = event.target;
+  alert("you bought :  " +title );
+}
+
+function handleUpdateBtnClick(event){
+  const {id} = event.target ;
+  console.log("update "+id)
+}
+
+
+function handleDeleteBtnClick(event){
+  const {id} = event.target;
+  console.log("Delete " + id)
+}
+
+
   return (
-    <div  >
-      {books.map(book  => 
-      <div >
-      <h1>{book.title}</h1>
-      <h2>{book.content}</h2>
-      <p>********************</p>
-        </div>  
-    )}
+    <div>
+        <Row md={4}>
+      {books.map((book) => (
+         <Col  md={3}>
+        <div className="div-container">
+        <Card  className='div-card' style={{ width: "18rem" }} >
+          <Card.Img className='div-card-img' variant="top" src={book.image} />
+          <Card.Body>
+            <Card.Title className='div-card-title'>{book.title}</Card.Title>
+            <Card.Text className='div-card-title'>Author: {book.author}</Card.Text>
+            <Card.Text className='div-card-title'>Description: {book.content}</Card.Text>
+            <Card.Text className='div-card-title' style={{ color:"red" }}>Price: {book.price} JOD</Card.Text>
+            <Button title={book.title} onClick={handleBuyBtnClick} className='div-card-button' variant="primary">Buy</Button>
+            <Button id={book._id} onClick={handleUpdateBtnClick} className='div-card-button' variant="primary">Update</Button>
+            <Button id={book._id} onClick={handleDeleteBtnClick} className='div-card-button' variant="primary">Delete</Button>
+          </Card.Body>
+        </Card>
+        </div>
+        </Col>
+      ))}
+      </Row>
     </div>
   );
-
-
-
-
-
-};
-
+}
 
 export default Books;
